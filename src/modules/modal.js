c9ec91b595dec5a10e89e55a1a4496ba4675ce1a
DaHbka <flexinzflexinz@gmail.com>
2022-02-28 20:21:22 +0300

    Modal rewrite

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,35 +1,38 @@
 const modal = () => {
     const modal = document.querySelector('.popup');
-    const modalWindow = modal.querySelector('.popup-content');
     const buttons = document.querySelectorAll('.popup-btn');
-    const btnClose = modal.querySelector('.popup-close');
-    let width = document.documentElement.clientWidth;
-    let id;
-
-    modal.style.transition = 'opacity 0.3s ease';
-    modal.style.opacity = 0;
 
     const changeDisplay = () => {
+        let width = document.documentElement.clientWidth;
+
         if (!modal.style.display) {
             modal.style.display = 'block';
+
             if (width > 768) setTimeout(() => (modal.style.opacity = 1), 100);
             else modal.style.opacity = 1;
         } else {
             modal.style.opacity = 0;
+
             if (width > 768) setTimeout(() => (modal.style.display = ''), 310);
             else modal.style.display = '';
         }
     };
 
+    modal.addEventListener('click', e => {
+        if (
+            !e.target.closest('.popup-content') ||
+            e.target.classList.contains('popup-close')
+        ) {
+            changeDisplay();
+        }
+    });
+
     buttons.forEach(btn => {
         btn.addEventListener('click', changeDisplay);
     });
 
-    btnClose.addEventListener('click', changeDisplay);
-
-    window.addEventListener(
-        'resize',
-        () => (width = document.documentElement.clientWidth)
-    );
+    modal.style.transition = 'opacity 0.3s ease';
+    modal.style.opacity = 0;
 };
+
 export default modal;
