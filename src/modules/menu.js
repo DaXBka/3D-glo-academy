9369e83a18cbeb855b910f5ef2440a273b7785c1
DaHbka <flexinzflexinz@gmail.com>
2022-03-03 23:39:23 +0300

    Menu fixed+

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -4,7 +4,7 @@ const menu = function () {
     console.log(main);
 
     document.addEventListener('click', e => {
-        if (e.target.closest('.menu') || e.target.matches('menu, menu>li')) {
+        if (e.target.closest('.menu') || e.target.matches('menu, menu li')) {
             menu.classList.add('active-menu');
         } else {
             menu.classList.remove('active-menu');
